9689601bf13934518cbc4cd826b0f8af84cd68db
Mohit Kushwah <ladduboykushwah@gmail.com>
2025-11-17 16:37:47 +0530

    Add files via upload

---
+++ Full stack/29-10-2025/script.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("inp")
+let button = document.getElementById("btn");
+let list = document.getElementById("list");
+
+function addTodo(){
+    let inpValue = input.value;
+    let li = document.createElement("li");
+    li.innerText = inpValue;
+
+    
+    
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.addEventListener("click", function(){
+        list.removeChild(li);
+    })
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+    input.value = "";
+
+}
+
+button.addEventListener("click", addTodo)
